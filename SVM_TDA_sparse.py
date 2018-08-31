b75613d8301a1d6959349575770a381e0ba06818
edelahah <johndoe@example.com>
2018-08-31 13:32:07 +0200

    Delete unused functions

--- SVM_TDA_sparse.py
+++ SVM_TDA_sparse.py
@@ -22,27 +22,3 @@ def test_tda_svm_sparse(X, Y, n, step, best, C):
         y[i] = single(X, Y, val, best, C)
         print("%.2f" % (i/len(y)*100), end="\t")
     return x, y
-
-#########
-# clean
-#########
-
-def single_clean(Xc, Yc, X, Y, n, best, C):
-    X_train = Xc[best[:n]]
-    Y_train = Yc[best[:n]]
-    U = X
-    T = Y
-    try:
-        svm = SVC(kernel='linear', C=C, class_weight="balanced")
-        svm.fit(X_train, Y_train)
-        return svm.score(U, T)
-    except:
-        return 0
-
-def test_tda_svm_sparse_clean(Xc, Yc, X, Y, n, step, best, C):
-    x = list(range(2, n, step))
-    y = np.zeros(len(x))
-    for i, val in enumerate(x):
-        y[i] = single_clean(Xc, Yc, X, Y, val, best, C)
-        print("%.2f" % (i/len(y)*100), end="\t")
-    return x, y
